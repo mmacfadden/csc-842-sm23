e40bd7df2f241b81e92ec564775d290e2ea0c0b0
Michael MacFadden <michael@macfadden.org>
2023-05-19 20:23:29 -0500

    Throw exceptions if code generator doesn't support a language.

--- cycle-2/src/codegen.py
+++ cycle-2/src/codegen.py
@@ -1,7 +1,7 @@
 class CodeGenerator():
+
+    def generate_js_code(self) -> str:
+        raise Exception(f"{self.__class__.__name__} does no support JavaScript outputs")
     
-    def generate_js_code(config) -> str:
-        pass
-    
-    def generate_py_code(config) -> str:
-        pass
+    def generate_py_code(self) -> str:
+        raise Exception(f"{self.__class__.__name__} does no support Python outputs")
