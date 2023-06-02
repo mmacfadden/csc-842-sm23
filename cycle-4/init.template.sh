f5e3d0f1d21f2069e8879b67e2f1313374ac965e
Michael MacFadden <michael@kali.home.macfadden.org>
2023-06-02 15:20:49 -0500

    Updated the init file generation.

--- cycle-4/init.template.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mount -t proc none /proc
-mount -t sysfs none /sys
-mount -t devtmpfs none /dev
-mount -t tmpfs none /tmp
-
-setuidgid 1000 /bin/sh
--- cycle-4/kernel-boot.py
+++ cycle-4/kernel-boot.py
@@ -164,7 +164,7 @@ def write_init(uid: int, root_fs_build_dir) -> str:
 
         setuidgid {uid} /bin/sh
         """
-    )
+    ).strip()
 
     with open(os.path.join(root_fs_build_dir, "init"), "w") as init_file:
         init_file.write(init)
@@ -180,7 +180,8 @@ def build_root_fs(root_fs_dir):
     exec(f"cp -av {busybox_source_dir}/_install/* {root_fs_build_dir}")
     exec("rm linuxrc", root_fs_build_dir)
     exec("mkdir proc sys dev tmp", root_fs_build_dir)
-    exec(f"cp init.template.sh {root_fs_build_dir}/init")
+    write_init(1000, root_fs_build_dir)
+    # exec(f"cp init.template.sh {root_fs_build_dir}/init")
     exec(f"chmod +x {root_fs_build_dir}/init")
 
     exec(f"find . | cpio -ov --format=newc > ../initramfs.cpio", root_fs_build_dir)
@@ -190,8 +191,7 @@ def boot(kernel_version):
     print("Booting Kernel")
 
     qemu_command = textwrap.dedent(
-        f"""
-        qemu-system-aarch64 \
+        f"""qemu-system-aarch64 \
         -M virt -m 1024 -cpu cortex-a53 \
         -kernel build/kernels/linux-{kernel_version}/arch/arm64/boot/Image.gz \
         -initrd build/initramfs.cpio \
