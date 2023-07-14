79f75f8f698861c87f96c000ad015b0046b1624f
Michael MacFadden <michael@macfadden.org>
2023-07-13 18:16:39 -0600

    Added email detection.

---
+++ cycle-10/docker/mariadb/4-email-account.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `email_account` (
+    `user` VARCHAR(255),
+    `email` VARCHAR(255)
+);
+
+INSERT INTO `email_account` VALUES("michael", "michael@example.org");
+INSERT INTO `email_account` VALUES("tony", "tony@example.org");
